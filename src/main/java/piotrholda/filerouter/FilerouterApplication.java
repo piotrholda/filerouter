1da5d9715cd6734482714bbcc398ce40a4be3d0d
Piotr Holda <pholda@gmail.com>
2019-09-28 15:29:10 +0200

    Initial commit

---
+++ src/main/java/piotrholda/filerouter/FilerouterApplication.java
@@ -0,0 +1,13 @@
+package piotrholda.filerouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilerouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilerouterApplication.class, args);
+	}
+
+}
